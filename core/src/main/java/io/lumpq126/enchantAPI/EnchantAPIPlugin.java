6a5e47abd984a8899386d8515be48055730f3be9
_LumPq_ <yeonggyu915@gmail.com>
2025-08-16 12:57:00 +0900

    Update EnchantAPIPlugin.java

--- core/src/main/java/io/lumpq126/enchantAPI/EnchantAPIPlugin.java
+++ core/src/main/java/io/lumpq126/enchantAPI/EnchantAPIPlugin.java
@@ -7,7 +7,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Level;
 
-public class EnchantAPIPlugin extends JavaPlugin {
+public final class EnchantAPIPlugin extends JavaPlugin {
     private static NMSHandler nms;
 
     @Override
